84cde4a26818437f2dcf8a79516db9a88d3e57a5
Daniel <dansanrjsdev@hotmail.com>
2022-12-15 16:56:16 -0500

    High Order Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/index.js';
+// import App from './App/index.js';
+
+function App(props) {
+  return (
+    <h1>¡{props.saludo}, {props.name}!</h1>
+  );
+}
+
+function withSaludo(WrappedComponent){
+  return function WrappedComponentewithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <p>Estamos acompañando al WrappedComponent</p>
+        </>
+      )
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo(App)('Wenas');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <AppWithSaludo name='Juanita'/>
+  // <App saludo = 'Buenas' nombre='Nath'/>
 );
